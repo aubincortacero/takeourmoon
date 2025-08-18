5246f7aaa814f7292a846b80c632234509f66aab
aubincortacero <contact@aubincortacero.fr>
2025-08-18 18:34:18 +0200

    Firt commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      wpApi: process.env.NUXT_PUBLIC_WP_API
+    }
+  }
+})
